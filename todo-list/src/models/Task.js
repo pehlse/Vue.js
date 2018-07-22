e94928008b71c9fdca4b244d94703be1b3a6490e
Pedro Henrique de Lima SIlva <pedro97henrique@gmail.com>
2018-07-21 22:16:46 -0300

    ok

---
+++ todo-list/src/models/Task.js
@@ -0,0 +1,7 @@
+export class Task {
+    constructor (){
+        this.id = '' //"5"
+        this.tittle = ''
+        this.completed = ''
+    }
+}
--- todo-list/src/router/index.js
+++ todo-list/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import App from '../src/App.vue'
 
 Vue.use(Router)
 
@@ -8,8 +8,7 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      component: App
     }
   ]
 })
