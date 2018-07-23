98b776234a0f0c4e80518c6f8c5ec9e96044bcea
Pedro Henrique de Lima SIlva <pedro97henrique@gmail.com>
2018-07-23 11:09:27 -0300

    Arquivo principal

--- calculator/src/App.vue
+++ calculator/src/App.vue
@@ -1,36 +1,37 @@
  <template>
      <div id="app">
-         <h1>Ola vue!</h1>
+         <h1>Calculator</h1><Calculator/>
      </div>
  </template>
  
  <script>
+ import Calculator from "./main/Calculator"
  export default {
-     
+    components:{Calculator}
  }
  </script>
  
  <style>
- @font-face {
-     font-family: "RobotoMono";
-     src: url("./font/RobotoMono-Thin.ttf");
- }
- *{
-     font-family: "RobotoMono",monospace;
- }
- body{
-     margin: 0;
- }
- #app {
-     display: flex;
-     flex-direction:  column;
-     height: 100vh;
-     justify-content: center;
-     align-items: center;
-     text-align: center;
+    @font-face {
+        font-family: "RobotoMono";
+        src: url("./font/RobotoMono-Thin.ttf");
+    }
+    *{
+        font-family: "RobotoMono", monospace;
+    }
+    body{
+        margin: 0;
+    }
+    #app{
+        display: flex;
+        flex-direction: column;
+        height: 100vh;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
 
-     color: #ffffff;
-     background: linear-gradient(to right, rgb(rgb(83, 105, 118),rgb(41, 46, 73)));
- }
+        color: #ffffff;
+        background: linear-gradient(to right, rgb(83,105,118), rgb(41,46,73));
+    }
  </style>
  
---
+++ calculator/src/main/Calculator.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="calculator">
+    <Display/>
+    <Button/>
+</div>
+    
+</template>
+
+<script>
+import Display from "../components/Display"
+import Button from "../components/Button"
+
+export default {
+    components:{Button, Display}
+}
+</script>
+    
+<style>
+.calculator{
+    height: 320px;
+    width: 235px;
+    border-radius: 5px;
+    overflow: hidden;
+
+    display: grid;
+    grid-template-columns: repeat(4,25%);
+    grid-template-rows: 1fr 48px 48px 48px 48px 48px;
+}
+</style>
