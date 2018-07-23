5f8d1eadd93dbdf97972dce10a7b6fdd9010e024
Pedro Henrique de Lima SIlva <pedro97henrique@gmail.com>
2018-07-23 00:45:38 -0300

    Projeto calculadora

--- calculator/src/App.vue
+++ calculator/src/App.vue
@@ -1,28 +1,36 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
+ <template>
+     <div id="app">
+         <h1>Ola vue!</h1>
+     </div>
+ </template>
+ 
+ <script>
+ export default {
+     
+ }
+ </script>
+ 
+ <style>
+ @font-face {
+     font-family: "RobotoMono";
+     src: url("./font/RobotoMono-Thin.ttf");
+ }
+ *{
+     font-family: "RobotoMono",monospace;
+ }
+ body{
+     margin: 0;
+ }
+ #app {
+     display: flex;
+     flex-direction:  column;
+     height: 100vh;
+     justify-content: center;
+     align-items: center;
+     text-align: center;
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+     color: #ffffff;
+     background: linear-gradient(to right, rgb(rgb(83, 105, 118),rgb(41, 46, 73)));
+ }
+ </style>
+ 
