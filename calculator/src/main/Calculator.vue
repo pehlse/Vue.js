206117658c9afc24a23517017e482993d1b37b44
Pedro Henrique de Lima SIlva <pedro97henrique@gmail.com>
2018-07-24 23:25:11 -0300

    Arquivo principal
    Arquivo principal do projeto calculadora contendo os metedos e a apresentação dos componentes criados

--- calculator/src/main/Calculator.vue
+++ calculator/src/main/Calculator.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="calculator">
-    <Display value="1000"/>
+    <Display :value="displayValue"/>
     <Button label="AC" triple @onClick="clearMemory"/>
     <Button label="/" operation @onClick="setOperation"/>
     <Button label="7" @onClick="addDigit"/>
@@ -42,11 +42,52 @@ export default {
         clearMemory(){
            Object.assign(this.$data, this.$options.data())
         },
+
         setOperation(operation){
-            
+            if (this.current === 0){
+                this.operation = operation
+                this.current = 1
+                this.clearDisplay = true
+            } else {
+                const equals = operation === "="
+                const currentOperation = this.operation
+
+                try{
+                    this.values[0] = eval(
+                        `${this.values[0]} ${currentOperation} ${this.values[1]}`
+                    )
+                }catch(e){
+                    this.$emit('onError', e)
+                }
+
+                this.values[1] = 0
+
+                this.displayValue = this.values[0]
+                this.operation = equals ? null : operation
+                this.current = equals ? 0 : 1
+                this.clearDisplay = !equals
+            }
         },
+
         addDigit(n){
+            if (n == "." && this.displayValue.includes(".")){ /*esta condição analisa se jaexiste um ponto (.) no display se ja houver ela retorna vazia*/          
+            return
+            }
+                                                        
+
+            const clearDisplay = this.displayValue === "0" || this.clearDisplay
+            const currentValue = clearDisplay ? "" : this.displayValue
+            const displayValue = currentValue + n
             
+            this.displayValue = displayValue
+            this.clearDisplay = false
+            
+            if (n !== "."){
+                const i = this.current
+                const newValue = parseFloat(displayValue)
+                this.values[i] = newValue
+            }
+            //console.log(n)
         }
     }
 }
