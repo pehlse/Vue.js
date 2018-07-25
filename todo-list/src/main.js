9b24fde312614a02c934e179a9603186f618dd98
Pedro Henrique de Lima SIlva <pedro97henrique@gmail.com>
2018-07-25 11:22:33 -0300

    Alterações nos arquivos principais

--- todo-list/src/main.js
+++ todo-list/src/main.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
+import App from './App'
 
 new Vue({
-  render: h => h(App)
-}).$mount('#app')
+    render: h => h(App)
+}).$mount("#app")
