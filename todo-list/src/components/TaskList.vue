0138e2fe9317bf54bf38e089d6a0268d6e8c8d1a
Pedro Henrique de Lima SIlva <pedro97henrique@gmail.com>
2018-07-27 22:58:57 -0300

    Mostra a lista de tarefas

--- todo-list/src/components/TaskList.vue
+++ todo-list/src/components/TaskList.vue
@@ -2,8 +2,9 @@
     <ul class="todo-list">
         <li v-for="todo in todoList" :key="todo.id" class="todo"> 
             <div class="view">
-                <label>{{todo.title}}</label>
-            </div>
+				<input class="toggle" @click="completeTask(todo)" type="checkbox">
+				<label  v-bind:class="{ 'todo-completed': todo.completed }" >{{ todo.title }}</label>
+			</div>
         </li>
     </ul>
 </template>
@@ -12,19 +13,26 @@
 export default {
     props:['todoList'],
     computed:{
-        sortedTasks: function(){
-            let sorted = this.todoList
-            return sorted.sort(function (a,b){
-                if (a.title < b.title) return -1
-                if (a.title > b.title) return 1
-                return 0
-            })
-        },
-        set: function (novaLista){
-            return this.todoList.concat(novaLista)
-        }
+        sortedTasks: {
+			get: function(){
+				let sorted = this.todoList
+				return sorted.sort(function(a,b){
+					if (a.title < b.title) return -1
+					if (a.title > b.title) return 1
+					return 0
+				})
+			}
+		} 
+		},
+	set :function(novaLista){
+		this.todoList.concat(novaLista)
+	},
+    methods:{
+		completeTask (task){
+			task.completed = !task.completed
+		}
+	} 
     }
-}
 </script>
 
 <style>
