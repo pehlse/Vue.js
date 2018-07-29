abdf31bd348fe62bd123046d28d7010b3b83c554
Pedro Henrique de Lima SIlva <pedro97henrique@gmail.com>
2018-07-29 16:41:45 -0300

    Arquivo de teste componente input

---
+++ todo-list/test/unit/specs/InputTask.spec.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import InputTask from 'src/components/InputTask'
+
+describe('InputTask.vue', () => {
+    it('should render correct contents', () => {
+        const Constructor = Vue.extend(InputTask)
+        const vm = new Constructor().$mount()
+        expect(vm.$el.querySelectorAll('.new-todo').lenght).to.equal(1)
+    })
+})
