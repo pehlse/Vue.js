f02f6d32f6975c7e3c11278f179d1496f5c4a48f
Pedro Henrique de Lima SIlva <pedro97henrique@gmail.com>
2018-07-30 22:43:01 -0300

    Componente para checar CEP

---
+++ todo-list/src/CepChecker.vue
@@ -0,0 +1,20 @@
+<template>
+<section class="cepChecker">
+    <label>Digite seu CEP</label>
+    <input type="text"/>
+</section>    
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
