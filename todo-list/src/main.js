b0c14c685d5d5fa9c5dcf8773b021c378e058d20
Pedro Henrique de Lima SIlva <pedro97henrique@gmail.com>
2018-07-30 22:43:14 -0300

    Main.js

--- todo-list/src/main.js
+++ todo-list/src/main.js
@@ -1,6 +1,23 @@
 import Vue from 'vue'
 import App from './App'
 
+//Router
+
+import VueRouter from 'vue-router'
+import routes from './routes'
+Vue.use(VueRouter)
+const router = new VueRouter ({
+    mode : 'history',
+    routes
+})
+
 new Vue({
-    render: h => h(App)
+    router,
+    render: h => h(App),
+    template : `
+    <div id="app">
+        <!-- component sera mostrado aqui -->
+        <router-view class="view"</router-view>
+    </div>
+    `
 }).$mount("#app")
